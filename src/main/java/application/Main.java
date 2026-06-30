11ea3918553561465b1f523c5188c6edc712fed4
Mateus <mf753161@gmail.com>
2026-06-30 15:07:50 -0300

    DepartmentDaoJDBC Crud Implementation

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -1,6 +1,7 @@
 package application;
 
 import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
 import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
@@ -47,6 +48,37 @@ public class Main {
             System.out.println("=== TEST 6: Seller delete ===");
             System.out.print("Enter id to delete: ");
             sellerDao.deleteById(sc.nextInt());
+            System.out.println("Delete completed!");
+
+            DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+
+            System.out.println();
+            System.out.println("=== TEST 7: Department findById ===");
+            Department dep = departmentDao.findById(1);
+            System.out.println(dep);
+
+            System.out.println();
+            System.out.println("=== TEST 8: Department findAll ===");
+            List<Department> depList = departmentDao.findAll();
+            depList.forEach(System.out::println);
+
+            System.out.println();
+            System.out.println("=== TEST 9: Department insert ===");
+            Department newDepartment = new Department(null, "Music");
+            departmentDao.insert(newDepartment);
+            System.out.println("Inserted! New id = " + newDepartment.getId());
+
+            System.out.println();
+            System.out.println("=== TEST 10: Department update ===");
+            dep = departmentDao.findById(1);
+            dep.setName("Food");
+            departmentDao.update(dep);
+            System.out.println("Update completed!");
+
+            System.out.println();
+            System.out.println("=== TEST 11: Department delete ===");
+            System.out.print("Enter id to delete: ");
+            departmentDao.deleteById(sc.nextInt());
             System.out.println("Delete completed!");
         }
     }
